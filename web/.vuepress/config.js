37de4164bb7156dcc1c4fb206d0733545e8bf50e
Min Si Thu <archimedes557@gmail.com>
2020-05-31 17:02:22 +0630

    feat: ga analytics

--- web/.vuepress/config.js
+++ web/.vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
     title:"MST Engine",
     description:"Programming Articles in Myanmar",
+    plugins: [
+        [
+          '@vuepress/google-analytics',
+          {
+            'ga': 'UA-148716119-2' // UA-00000000-0
+          }
+        ]
+      ]
 }
